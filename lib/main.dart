11cce068ed2dadb053768f4918ef875bfa46f307
s6135410006 <s6135410006@sau.ac.th>
2021-08-25 17:41:39 +0700

    code v1.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/views/first_ui.dart';
+import 'package:flutter_application_1/views/second_ui.dart';
 
 void main() {
   runApp(
